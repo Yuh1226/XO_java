a5b77690cc7ca540cf028469bf62a00d8f25a0fc
Yuh1226 <huyga2014@gmail.com>
2025-05-04 00:51:22 +0700

    Chỉnh sửa hôm 4/5/25

--- src/XO/Control.java
+++ src/XO/Control.java
@@ -1,7 +1,9 @@
 package XO;
 
+import java.util.Stack;
+
 public class Control {
-	private Game game;
+    private Game game;
     private View view;
 
     public Control(Game game, View view) {
@@ -11,5 +13,66 @@ public class Control {
 
     public void run() {
         view.displayBoard(game.getBoard());
+        Stack<Move> undoStack = game.getUndoStack();
+        Stack<Move> redoStack = game.getRedoStack();
+
+        int s = 0;
+        while (!game.isFull()) {
+            int option = view.getCommand();
+            if (option == 1) { // nhập tọa độ
+                s = s + 1;
+                int[] input = view.getInput(s);
+                int x = input[0];
+                int y = input[1];
+                int value = input[2];
+
+                if (x >= 0 && x < 9 && y >= 0 && y < 9) {
+                    if (!game.isValidMove(x, y)) { // kiểm tra nước không hợp lệ
+                        view.showMessage("Nước đi không hợp lệ! Vui lòng thử lại.");
+                        view.displayBoard(game.getBoard());
+                    } else {
+                        int oldValue = game.getBoard()[x][y].getValue();
+                        game.getBoard()[x][y].setValue(value);
+                        undoStack.push(new Move(x, y, oldValue, value));
+                        redoStack.clear();
+                        
+                        view.displayBoard(game.getBoard());
+                    }
+
+                    if (s >= 9 && game.checkWinner() == 1) {
+                        view.showMessage("Người chơi x thắng");
+                    } else if (s >= 9 && game.checkWinner() == -1) {
+                        view.showMessage("Người chơi o thắng");
+                    }
+                } else {
+                    view.showMessage("Đầu vào không hợp lệ! Vui lòng nhập lại.");
+                }
+            } else if (option == 2) { // undo
+                if (undoStack.isEmpty()) {
+                    view.showMessage("Không có thao tác để hoàn tác!");
+                } else {
+                    Move move = undoStack.pop();
+                    game.getBoard()[move.getRow()][move.getCol()].setValue(move.getOldValue());
+                    redoStack.push(move);
+                    view.showMessage("Đã hoàn tác!");
+                    view.displayBoard(game.getBoard());
+                }
+            } else if (option == 3) { // redo
+                if (redoStack.isEmpty()) {
+                    view.showMessage("Không có thao tác để làm lại!");
+                } else {
+                    Move move = redoStack.pop();
+                    game.getBoard()[move.getRow()][move.getCol()].setValue(move.getNewValue());
+                    undoStack.push(new Move(move.getRow(), move.getCol(), move.getOldValue(), move.getNewValue()));
+                    view.showMessage("Đã làm lại!");
+                    view.displayBoard(game.getBoard());
+                }
+            } else if (option == 4) { // thoát
+                view.showMessage("Đã thoát game.");
+                return;
+            } else {
+                view.showMessage("Lệnh không hợp lệ! Vui lòng thử lại.");
+            }
+        }
     }
 }
