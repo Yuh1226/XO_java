3a22cb86516d329c67b828a93c91718ceb3519d1
Yuh1226 <ADMIN@localhost>
2025-05-06 23:14:20 +0700

    sửa khoảng cách giữa các lênh

--- X_O_game/src/XO/Node.java
+++ X_O_game/src/XO/Node.java
@@ -4,35 +4,42 @@ public class Node {
 	private int x;
 	private int y;
 	private int value;
-	
+
 	public Node(int x, int y, int value) {
 		this.x = x;
 		this.y = y;
 		this.value = value;
 	}
-	
+
 	public int getX() {
 		return x;
 	}
+
 	public void setX(int x) {
-		return this.x = x;
+		this.x = x;
 	}
-	
+
 	public int getY() {
 		return y;
 	}
+
 	public void setY(int y) {
-		return this.y = y;
+		this.y = y;
 	}
-	
+
 	public int getValue() {
 		return value;
 	}
-	public void setValue(char value) {
-		return this.value = value;
+
+	public void setValue(int value) {
+		this.value = value;
 	}
-	
+
 	public String toString() {
-        return value == 0 ? " " : String.valueOf(value);
-    }
+		if (value == 0)
+			return " ";
+		else if (value == 1)
+			return "X";
+		return "O";
+	}
 }
