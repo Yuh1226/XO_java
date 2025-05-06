2277766128d03a955284eff33ab3fad5c005bf14
Yuh1226 <ADMIN@localhost>
2025-05-06 23:47:23 +0700

    sủa lần 2 (6/5/2025)

---
+++ X_O_game/src/XO/View.java
@@ -0,0 +1,60 @@
+package XO;
+
+import java.util.Scanner;
+
+public class View {
+	private Scanner scanner;
+
+	public View() {
+		scanner = new Scanner(System.in);
+	}
+
+	public void displayBoard(Node[][] board) {
+		System.out.println("X/O Board:");
+		System.out.println(" ─── ─── ─── ─── ─── ─── ─── ─── ───");
+		for (int i = 0; i < 9; i++) {
+			System.out.print("|");
+			for (int j = 0; j < 9; j++) {
+				System.out.print(" " + board[i][j]);
+				if (j < 8)
+					System.out.print(" |");
+				else
+					System.out.print(" |");
+			}
+			System.out.println();
+			System.out.print(" ");
+			if (i < 8)
+				System.out.println("─── ─── ─── ─── ─── ─── ─── ─── ─── ");
+		}
+		System.out.println("─── ─── ─── ─── ─── ─── ─── ─── ───");
+	}
+
+	public int getCommand() {
+		System.out.println();
+		System.out.println("====== GAME X/O MENU ======");
+		System.out.println("1. Play");
+		System.out.println("2. Undo ");
+		System.out.println("3. Redo ");
+		System.out.print("Nhấn lựa chọn, nhấn bất kì để thoát: ");
+		// System.out.print("Option: ");
+		int a = scanner.nextInt();
+		System.out.println("==========================\n");
+		return a;
+	}
+
+	public int[] getInput(int index) {
+		System.out.print("Nhập toạ độ(X, y): ");
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		int value;
+		if (index % 2 != 0)
+			value = 1;
+		else
+			value = -1;
+		return new int[] {x, y, value};
+	}
+
+	public void showMessage(String message) { // chạy thông báo
+		System.out.println(message);
+	}
+}
